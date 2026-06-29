12e261da27d717d171d3d147baf7edadacfe95d1
araxnoid-code <araxnoid0@gmail.com>
2026-06-29 19:14:47 +0700

    added an operation to eliminate the need to insert backward labels with a value of None into the queue on the backward

--- src/pzeudo/module/backward/backward.rs
+++ src/pzeudo/module/backward/backward.rs
@@ -14,23 +14,43 @@ pub trait Backward<'a>: TensorTrait<'a> {
                     match &*backward_label {
                         BackwardLabel::Add(lhs, rhs) => {
                             add_backward(&lhs.1, &rhs.1, &gradient);
-                            queue.push((lhs.2.clone(), lhs.1.clone()));
-                            queue.push((rhs.2.clone(), rhs.1.clone()));
+                            if let Some(_) = &lhs.2 {
+                                queue.push((lhs.2.clone(), lhs.1.clone()));
+                            }
+
+                            if let Some(_) = &rhs.2 {
+                                queue.push((rhs.2.clone(), rhs.1.clone()));
+                            }
                         }
                         BackwardLabel::Sub(lhs, rhs) => {
                             sub_backward(&lhs.1, &rhs.1, &gradient);
-                            queue.push((lhs.2.clone(), lhs.1.clone()));
-                            queue.push((rhs.2.clone(), rhs.1.clone()));
+                            if let Some(_) = &lhs.2 {
+                                queue.push((lhs.2.clone(), lhs.1.clone()));
+                            }
+
+                            if let Some(_) = &rhs.2 {
+                                queue.push((rhs.2.clone(), rhs.1.clone()));
+                            }
                         }
                         BackwardLabel::Mul(lhs, rhs) => {
                             mul_backward(lhs.0.view(), &lhs.1, rhs.0.view(), &rhs.1, &gradient);
-                            queue.push((lhs.2.clone(), lhs.1.clone()));
-                            queue.push((rhs.2.clone(), rhs.1.clone()));
+                            if let Some(_) = &lhs.2 {
+                                queue.push((lhs.2.clone(), lhs.1.clone()));
+                            }
+
+                            if let Some(_) = &rhs.2 {
+                                queue.push((rhs.2.clone(), rhs.1.clone()));
+                            }
                         }
                         BackwardLabel::Div(lhs, rhs) => {
                             div_backward(lhs.0.view(), &lhs.1, rhs.0.view(), &rhs.1, &gradient);
-                            queue.push((lhs.2.clone(), lhs.1.clone()));
-                            queue.push((rhs.2.clone(), rhs.1.clone()));
+                            if let Some(_) = &lhs.2 {
+                                queue.push((lhs.2.clone(), lhs.1.clone()));
+                            }
+
+                            if let Some(_) = &rhs.2 {
+                                queue.push((rhs.2.clone(), rhs.1.clone()));
+                            }
                         }
                     }
                 }

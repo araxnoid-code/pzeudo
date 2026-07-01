0c68136c2bf43157d2b2fb2c04267daa727ae5d7
araxnoid-code <araxnoid0@gmail.com>
2026-07-01 11:03:14 +0700

    added broadcasting handling to fn sub_backward()

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let tensor_a = Tensor::from_array(ArrayD::<f32>::zeros(vec![2, 1, 5]));
     let tensor_b = Tensor::from_array(ArrayD::<f32>::zeros(vec![1, 2, 3, 5]));
 
-    let tensor_c = tensor_a.add(&tensor_b, &mut record);
+    let tensor_c = tensor_b.sub(&tensor_a, &mut record);
 
     tensor_c.backward(&mut record);
 
